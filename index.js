9226c333224c7592ac562c722cfdb19ba491cdaa
eagyemang21 <elijahagyemang9@gmail.com>
2021-10-06 14:45:32 +0000

    code challenge 10/6/2021

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
-//1
-function absoluteValue(){
-  
-}
+//1  //if the argument is a positive number, return the value.  
+     //if the argument is a negative number, return the positive integer.
+     //if the argument is not an integer, return "null"
+    //typeof function
+function absoluteValue(number){
+    if (typeof number === "number"){
+    if(number < 0){
+        return (number * -1);
+    }else{
+        return number;
+    }
+    }else{
+        return null
+    }
+   }
 
-
-
-
+console.log(absoluteValue(20))
